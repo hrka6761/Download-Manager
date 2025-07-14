4e996496335fd4a589cf626809e279acf4a19029
HAMIDREZA KARAMI <hrka6761@gmail.com>
2025-07-14 14:08:33 +0330

    Set app version and minsdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,10 +10,10 @@ android {
 
     defaultConfig {
         applicationId = "ir.hrka.downloadmanager"
-        minSdk = 33
+        minSdk = 31
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
